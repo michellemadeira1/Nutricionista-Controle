5e5109542db3d4fd63ba0ba412f1c098ecd70894
Michelle Madeira <michellemadeira1@gmail.com>
2024-10-08 15:28:28 -0300

    first commit

---
+++ Nutricionista-back/src/main/java/com/Nutricionista/Nutricionista/NutricionistaApplication.java
@@ -0,0 +1,13 @@
+package com.Nutricionista.Nutricionista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutricionistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NutricionistaApplication.class, args);
+	}
+
+}
---
+++ Nutricionista-back/src/test/java/com/Nutricionista/Nutricionista/NutricionistaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Nutricionista.Nutricionista;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutricionistaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
