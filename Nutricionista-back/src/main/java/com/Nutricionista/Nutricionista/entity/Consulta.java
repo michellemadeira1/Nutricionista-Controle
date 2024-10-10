ecef05538e821d4cdad5b2db24cc7f7b54d68ab1
Michelle Madeira <michellemadeira1@gmail.com>
2024-10-10 15:03:06 -0300

    Entity-consulta

---
+++ Nutricionista-back/src/main/java/com/Nutricionista/Nutricionista/entity/Consulta.java
@@ -0,0 +1,65 @@
+package com.Nutricionista.Nutricionista.entity;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Consulta {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    private LocalDate data;
+    private LocalTime hora;
+    
+    
+    
+    
+    public Consulta() {}
+    
+	public Consulta(Long id, LocalDate data, LocalTime hora) {
+		super();
+		this.id = id;
+		this.data = data;
+		this.hora = hora;
+	}
+
+	
+	
+	
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public LocalTime getHora() {
+		return hora;
+	}
+
+	public void setHora(LocalTime hora) {
+		this.hora = hora;
+	}
+
+
+
+}
